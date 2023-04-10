692ef07a4e003cd4438e1726a7799bef9db874b7
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-10 11:04:54 +0100

    card section added

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {SiHiveBlockchain, SiStrapi, SiFsecure} from 'react-icons/si'
 import {VscServerProcess} from 'react-icons/vsc'
+import AboutCard from './AboutCard'
 
 const About = () => {
   return (
@@ -11,12 +12,13 @@ const About = () => {
            <p className='py-4 text-xl'>
                The Defi protocol system empowers developers, liquidity providers, and traders to participate in a financial marketplace that is open and accessible to all.
            </p>
-           <div className='grid grid-cols-4 gap-4'>
+           <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
 
-               <p>Card</p>
-               <p>Card</p>
-               <p>Card</p>
-               <p>Card</p>
+                <AboutCard icons={<SiHiveBlockchain size={40} />} heading = 'Reliable, tamper-proof network' text= "Use decentralization, trusted nodes, premium data, and cryptographic proofs to connect highly accurate and available data/APIs to any smart contract." />
+                <AboutCard icons={<SiFsecure size={40} />} heading = 'Seamless connection to any API' text= "Build on a flexible framework that can retrieve data from any API, connect with existing systems, and integrate with any current or future blockchain." />
+                <AboutCard icons={<SiStrapi size={40} />} heading = 'Proven, ready-made solutions' text= "Integrate pre-built, time-tested oracle solutions that already secure tens of billions in smart contract value for market-leading decentralized applications." />
+                <AboutCard icons={<VscServerProcess size={40} />} heading = 'Secure off-chain computation' text= "Use a decentralized network of DeFi Keeper nodes to automate contracts, mitigating risk of manual interventions and centralized servers." />
+
            </div>
        </div>
     </div>
